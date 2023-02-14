3513ef1312e8fc700b1f77370e64af3f42ca6497
limoncello7 <zceexdo@ucl.ac.uk>
2023-02-14 01:38:36 +0000

    ready for first call back app

--- exaple.py
+++ exaple.py
@@ -21,6 +21,18 @@ WTI_MONTHLY_FILEPATH = Path(__file__).parent.joinpath( 'wti-month.csv')
 cols = ['Date', 'Price']
 df_brent_daily = pd.read_csv(BRENT_DAILY_FILEPATH, usecols=cols)
 df_wti_daily = pd.read_csv(WTI_DAILY_FILEPATH, usecols=cols)
+df_gas_daily = pd.read_csv(GAS_DAILY_FILEPATH, usecols=cols)
+df_gas_monthly = pd.read_csv(GAS_MONTHLY_FILEPATH, usecols=cols)
+df_wti_monthly = pd.read_csv(WTI_MONTHLY_FILEPATH, usecols=cols)
+df_brent_monthly = pd.read_csv(BRENT_MONTH_FILEPATH, usecols=cols)
+
+
+
+
+
+
+
+
 
 line_brent_daily = px.line(df_brent_daily, x='Date', y='Price', title='Brent Daily Price')
 
@@ -30,6 +42,7 @@ line_brent_daily.update_layout(title="Prices Over Time")
 fig_1 = px.line()
 fig_1.add_scatter(x=df_brent_daily['Date'], y=df_brent_daily['Price'], name='Brent Price')
 fig_1.add_scatter(x=df_wti_daily['Date'], y=df_wti_daily['Price'], name='WTI Price')
+
 
 
 app = Dash(__name__,
@@ -50,6 +63,8 @@ app.layout = dbc.Container(
             id='line-daily',
             figure=fig_1
         ),
+        html.H2("This chart is showing the trend of brent price changes compare to the change of gas price"),
+
     ],
     fluid=True,
 )
