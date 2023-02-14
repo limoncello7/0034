dc0514ae119f1161af1eb79df357beca590d88cc
limoncello7 <zceexdo@ucl.ac.uk>
2023-02-14 17:23:41 +0000

    Second graph now can be choosed by user themselves

--- exaple.py
+++ exaple.py
@@ -1,9 +1,10 @@
 """ This version creates all the charts in the main app file rather than in create_charts.py"""
 from pathlib import Path
 import pandas as pd
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, State, MATCH
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objs as go
 
 
 #-------
@@ -16,7 +17,8 @@ BRENT_MONTH_FILEPATH = Path(__file__).parent.joinpath('data','brent-month.csv')
 GAS_MONTHLY_FILEPATH = Path(__file__).parent.joinpath( 'data','gas-monthly.csv')
 WTI_DAILY_FILEPATH = Path(__file__).parent.joinpath( 'data','wti-daily.csv')
 WTI_MONTHLY_FILEPATH = Path(__file__).parent.joinpath( 'data','wti-month.csv')
-
+MONTHLY_FILEPATH = Path(__file__).parent.joinpath( 'monthly_file.csv')
+DAILY_FILEPATH = Path(__file__).parent.joinpath( 'daily_file.csv')
 
 cols = ['type','Date', 'Price']
 df_brent_daily = pd.read_csv(BRENT_DAILY_FILEPATH, usecols=cols)
@@ -25,18 +27,27 @@ df_gas_daily = pd.read_csv(GAS_DAILY_FILEPATH, usecols=cols)
 df_gas_monthly = pd.read_csv(GAS_MONTHLY_FILEPATH, usecols=cols)
 df_wti_monthly = pd.read_csv(WTI_MONTHLY_FILEPATH, usecols=cols)
 df_brent_monthly = pd.read_csv(BRENT_MONTH_FILEPATH, usecols=cols)
-
-
+df_monthly= pd.read_csv(MONTHLY_FILEPATH, usecols=cols)
+df_daily= pd.read_csv(DAILY_FILEPATH, usecols=cols)
 
 line_brent_daily = px.line(df_brent_daily, x='Date', y='Price', title='Brent Daily Price')
 
 line_wti_daily = px.line(df_wti_daily, x='Date', y='Price',title='WTI Daily Price')
 
+line_gas_daily = px.line(df_gas_daily, x='Date', y='Price',title='Gas Daily Price')
+
 line_brent_daily.update_layout(title="Prices Over Time")
+
 fig_1 = px.line()
 fig_1.add_scatter(x=df_brent_daily['Date'], y=df_brent_daily['Price'], name='Brent Price')
 fig_1.add_scatter(x=df_wti_daily['Date'], y=df_wti_daily['Price'], name='WTI Price')
+fig_1.add_scatter(x=df_gas_daily['Date'], y=df_gas_daily['Price'], name='Gas Price')
 
+
+
+df = px.data.gapminder()
+default_column_x = "gas"
+options = ["gas", "wti", "brent"]
 
 
 app = Dash(__name__,
@@ -50,18 +61,76 @@ app = Dash(__name__,
 )
 
 app.layout = dbc.Container(
-    [
-        html.H1("Dashboard of oil and gas prices"),
-        html.H2("This chart is showing how Brent&WTI oil price changes"),
-        dcc.Graph(
-            id='line-daily',
-            figure=fig_1
-        ),
-        html.H2("This chart is showing the trend of brent price changes compare to the change of gas price"),
-        
-    ],
-    fluid=True,
+[
+html.H1("Dashboard of Oil and Gas Prices"),
+html.P("This chart shows the changes in Brent & WTI oil prices."),
+dcc.Graph(id='line-daily', figure=fig_1),
+html.P("This chart shows the trend of Brent price changes compared to gas price changes."),
+html.Div(
+[
+html.H1(children='Pattern Matching Graphs'),
+dcc.Dropdown(
+id='category-dropdown',
+options=[
+{'label': 'Gas', 'value': 'gas'},
+{'label': 'WTI', 'value': 'wti'},
+{'label': 'Brent', 'value': 'brent'}
+],
+value='gas'
+),
+dcc.RadioItems(
+id='time-radio',
+
+value='daily'
+),
+dcc.Graph(id='pattern-matching-graph')
+],
+className="container-fluid"
+),
+],
+fluid=True,
 )
+@app.callback(
+    Output('pattern-matching-graph', 'figure'),
+    [Input('category-dropdown', 'value'),
+     Input('time-radio', 'value')]
+)
+def update_graph(category, time):
+    if category == 'gas':
+        df = df_gas_daily
+        title = 'Gas Prices'
+    elif category == 'wti':
+        df = df_wti_daily
+        title = 'WTI Prices'
+    else:
+        df = df_brent_daily
+        title = 'Brent Prices'
+
+    if time == 'daily':
+        x = df['Date']
+        y = df['Price']
+    elif time == 'weekly':
+        x = df['Week']
+        y = df['Price']
+    else:
+        x = df['Month']
+        y = df['Price']
+
+    return {
+        'data': [
+            go.Scatter(
+                x=x,
+                y=y,
+                mode='lines+markers'
+            )
+        ],
+        'layout': go.Layout(
+            title=title,
+            xaxis={'title': 'Date' if time == 'daily' else time.title()},
+            yaxis={'title': 'Price ($/bbl)'}
+        )
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
